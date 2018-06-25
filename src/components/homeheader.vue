4a29bc260c0d1658b6b5f5fde6066e147fce8aae
Zjiawen <1049394252@qq.com>
2018-06-25 21:53:06 +0800

    修改main.js

---
+++ src/components/homeheader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="page">
+        
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  store,
-  components: { App },
-  template: '<App/>'
+  template: `
+  <router-view></router-view>
+  `
 })
