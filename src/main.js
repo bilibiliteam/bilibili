15c53a153ca5a8f5f971da8c2200046de8efd998
17607843215 <xj7843215@Gmail.com>
2018-06-26 14:35:42 +0800

    indexBottom indexCommon

--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
-import Vuex from 'vuex'
-import store from './vuex/store.js'
 import '../static/font/iconfont.css'
+import '../static/css/base.css'
 
-Vue.use(Vuex)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  store,
-  components: { App },
-  template: '<App/>'
+  template: `
+    <router-view></router-view>
+  `
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import xIndex from '@/components/xIndex/xIndex'
 
 Vue.use(Router)
 
@@ -8,8 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: xIndex
     }
   ]
 })
