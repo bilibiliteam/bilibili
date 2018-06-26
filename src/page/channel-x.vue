8aeaa9a2309e6648743683b4c1d8e4c24167f79d
Zjiawen <1049394252@qq.com>
2018-06-26 17:14:25 +0800

    还原page文件夹

---
+++ src/page/channel-x.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="channel">
+    <homeHeader></homeHeader>
+    <xIndexBottom></xIndexBottom>
+  </div>
+
+</template>
+
+<script>
+  import homeHeader from '@/components/homeheader'
+  import xIndexBottom from '@/components/xIndex/xIndexBottom'
+    export default {
+        components:{
+          homeHeader,
+          xIndexBottom
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
