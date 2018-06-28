a5ef94424faf80eb00538ff082ae98525ddf5737
Zjiawen <1049394252@qq.com>
2018-06-28 17:11:17 +0800

    追番页面完善（未完成）

--- src/components/zhuifan/zhuifanContent.vue
+++ src/components/zhuifan/zhuifanContent.vue
@@ -3,7 +3,9 @@
         <div class="block-all" v-for="item in data">
             <div class="block-top">
                 <div class="left">
-                    <img :src="item.imgSrc" alt="" />
+                    <div>
+                        <img :src="item.imgSrc" alt="" />
+                    </div>
                     <p>{{item.itemName}}</p>
                 </div>
                 <div class="right">
@@ -20,8 +22,53 @@
             </div>
             <div class="bottom-block">
                 <img :src="item.btImg" alt="" />
-                <p class="item.title"></p>
-                <p class="jitem.lianjie"></p>
+                <p class="title">{{item.title}}</p>
+                <p class="jianjie">{{item.jianjie}}</p>
+            </div>
+        </div>
+        <div class="dptj block-all">
+            <div class="block-top">
+                <div class="left">
+                    <div class="pic">
+                        <img :src="dptjSrc" alt="" />
+                    </div>
+                    <p>点评推荐</p>
+                </div>
+                <div class="right">
+                    <p>查看更多 &gt;</p>
+                </div>
+            </div>
+            <div class="dptj-content" v-for="item in dptjData">
+                <div class="dptj-content-top">
+                    <div class="text">
+                        <h3 class="title">{{item.title}}</h3>
+                        <p class="text-content">{{item.content}}</p>
+                    </div>
+                    <div class="t-right">
+                        <img :src="item.imgSrc" alt="" />
+                    </div>
+                </div>
+                <div class="dptj-content-bottom">
+                    <div class="writer">
+                        <img class="writer-pic" :src="item.writePic" />
+                        <div class="writer-name">{{item.name}}</div>
+                    </div>
+                    <div class="like-comment">
+                        <div class="like">
+                            <i class="iconfont icon-iconfontzhizuobiaozhun44"></i>
+                            <span class="like-nums">{{item.likeNums}}</span>
+                        </div>
+                        <div class="comment">
+                            <i class="iconfont icon-xinxi"></i>
+                            <span class="comment-nums">{{item.commentNums}}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bjtj">
+            <div class="bjtj-top">
+                
             </div>
         </div>
     </div>
@@ -56,6 +103,8 @@
                                 curNum: '看到第 24 话'
                             }
                         ],
+                        title: '',
+                        jianjie: '',
                         btImg: '../../static/imgs/zhuifan10.jpg'
                     },
                     {
@@ -82,10 +131,42 @@
                                 curNum: '更新至25话'
                             }
                         ],
+                        title: '【资讯档】 2018年第25周',
+                        jianjie: '',
                         btImg: '../../static/imgs/zhuifan11.jpg'
                     }
                 ],
-                        
+                dptjSrc: '../../static/imgs/zhuifan3.jpg' ,
+                dptjData: [
+                    {
+                        title: '致这残酷美丽的世界',
+                        content: '灰与幻想的格林加姆尔，一个残酷而美丽的世界。一如既往的穿越，一如既往的异世界，一如既往的公会',
+                        imgSrc: 'https://i0.hdslb.com/bfs/bangumi/7fb4945f864e9e988212b7d20f206df2677053f8.jpg@180w_240h.webp',
+                        writePic: '../../static/imgs/zhuifan-dptj-user1.jpg',
+                        name: 'MHZJX',
+                        likeNums: 906,
+                        commentNums: 101
+                    },
+                    {
+                        title: '致这残酷美丽的世界',
+                        content: '灰与幻想的格林加姆尔，一个残酷而美丽的世界。一如既往的穿越，一如既往的异世界，一如既往的公会',
+                        imgSrc: 'https://i0.hdslb.com/bfs/bangumi/7fb4945f864e9e988212b7d20f206df2677053f8.jpg@180w_240h.webp',
+                        writePic: '../../static/imgs/zhuifan-dptj-user1.jpg',
+                        name: 'MHZJX',
+                        likeNums: 906,
+                        commentNums: 101
+                    },
+                    {
+                        title: '致这残酷美丽的世界',
+                        content: '灰与幻想的格林加姆尔，一个残酷而美丽的世界。一如既往的穿越，一如既往的异世界，一如既往的公会',
+                        imgSrc: 'https://i0.hdslb.com/bfs/bangumi/7fb4945f864e9e988212b7d20f206df2677053f8.jpg@180w_240h.webp',
+                        writePic: '../../static/imgs/zhuifan-dptj-user1.jpg',
+                        name: 'MHZJX',
+                        likeNums: 906,
+                        commentNums: 101
+                    }
+                ],
+
             }
         }
     }
@@ -110,7 +191,6 @@
                     color: #333;
                     img{
                         width: 26px;
-                        height: 26px;
                     }
                 }
                 .right{
@@ -140,7 +220,7 @@
                         text-overflow: ellipsis;
                         display: -webkit-box;
                         -webkit-line-clamp: 2;
-                        -webkit-box-orient: vertical
+                        -webkit-box-orient: vertical;
                     }
                     .same{
                         color: #999;
@@ -171,7 +251,81 @@
                     overflow: hidden;
                 }
             }
-
+        }
+        .dptj{
+            padding: 0;
+            font-size: 14px;
+            .block-top{
+                padding: 0 2%;
+                .left{
+                    .pic{
+                        img{
+                            width: 24px;
+                        }
+                    }
+                }
+            }
+            .dptj-content{
+                padding: 10px 2% 0;
+                border-bottom: 1px solid #ccc;
+                .dptj-content-top{
+                    display: flex;
+                    justify-content: space-between;
+                    .text{
+                        width: 76%;
+                        .title{
+                            line-height: 34px;
+                        }
+                        .text-content{
+                            font-size: 12px;
+                            line-height: 20px;
+                            max-height: 40px;
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                            display: -webkit-box;
+                            -webkit-line-clamp: 2;
+                            -webkit-box-orient: vertical;
+                        }
+                    }
+                    .t-right{
+                        width: 16%;
+                       img{
+                            width: 100%;
+                            border-radius: 5px;
+                        } 
+                    }
+                }
+                .dptj-content-bottom{
+                    color: #888;
+                    height: 40px;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    .writer{
+                        width: 30%;
+                        display: flex;
+                        align-items: center;
+                        .writer-pic{
+                            width: 20px;
+                            height: 20px;
+                            border-radius: 50%;
+                        }
+                        .writer-name{
+                            padding-left: 6px;
+                        }
+                    }
+                    .like-comment{
+                        width: 40%;
+                        display: flex;
+                        .like{
+                            flex: 1;
+                        }
+                        .comment{
+                            flex: 1;
+                        }
+                    }
+                }
+            }
         }
     }
 </style>
